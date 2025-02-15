d20bfbe7a6a6dde8408c11f373c310035bec27cc
Oleg <oleg.marinin.49@yandex.ru>
2025-02-15 15:49:28 +0300

    task A + tests Status OK

--- src/main/java/ru/marinin/training60_lesson1/A.java
+++ src/main/java/ru/marinin/training60_lesson1/A.java
@@ -1,4 +1,31 @@
 package ru.marinin.training60_lesson1;
 
+import java.util.Scanner;
+
 public class A {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        System.out.println(findNearest(x1, y1, x2, y2, x, y));
+    }
+
+    public static String findNearest(int x1, int y1, int x2, int y2, int x, int y) {
+
+        String ans = "";
+
+        if (y > y2) ans += "N";
+        if (y < y1) ans += "S";
+        if (x < x1) ans += "W";
+        if (x > x2) ans += "E";
+
+        return ans;
+    }
 }
--- src/test/java/ru/marinin/TestA.java
+++ src/test/java/ru/marinin/TestA.java
@@ -1,4 +1,108 @@
 package ru.marinin;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.marinin.training60_lesson1.A;
+
 public class TestA {
+
+    @Test
+    @DisplayName("NW")
+    void testNW() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = -4;
+        int y = 6;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "NW");
+    }
+
+    @Test
+    @DisplayName("N")
+    void testN() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = -1;
+        int y = 6;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "N");
+    }
+
+    @Test
+    @DisplayName("NE")
+    void testNE() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = 8;
+        int y = 6;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "NE");
+    }
+
+    @Test
+    @DisplayName("E")
+    void testE() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = 8;
+        int y = 2;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "E");
+    }
+
+    @Test
+    @DisplayName("SE")
+    void testSE() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = 8;
+        int y = -6;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "SE");
+    }
+
+    @Test
+    @DisplayName("S")
+    void testS() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = 2;
+        int y = -6;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "S");
+    }
+
+    @Test
+    @DisplayName("SW")
+    void testSW() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = -5;
+        int y = -6;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "SW");
+    }
+
+    @Test
+    @DisplayName("W")
+    void testW() {
+        int x1 = -1;
+        int y1 = -2;
+        int x2 = 5;
+        int y2 = 3;
+        int x = -6;
+        int y = 0;
+        Assertions.assertEquals(A.findNearest(x1, y1, x2, y2, x, y), "W");
+    }
+
+
 }
