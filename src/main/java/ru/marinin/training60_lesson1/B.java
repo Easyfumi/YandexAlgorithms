889979847e105f6a57a1618879832fe14d8a61cd
Oleg <oleg.marinin.49@yandex.ru>
2025-02-15 16:30:55 +0300

    WA with (TBlue > TRed && SBlue > SRed)

--- src/main/java/ru/marinin/training60_lesson1/B.java
+++ src/main/java/ru/marinin/training60_lesson1/B.java
@@ -1,4 +1,32 @@
 package ru.marinin.training60_lesson1;
 
+import java.util.Scanner;
+
 public class B {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int TBlue = scanner.nextInt();
+        int TRed = scanner.nextInt();
+        int SBlue = scanner.nextInt();
+        int SRed = scanner.nextInt();
+
+        System.out.println(findMin(TBlue, TRed, SBlue, SRed));
+    }
+
+    public static String findMin(int TBlue, int TRed, int SBlue, int SRed) {
+        String ans = "";
+        if (TBlue > TRed && SBlue > SRed) {
+            ans += (Math.min(TBlue, TRed) + 1) + " " + (Math.min(SBlue, SRed) + 1);
+        } else if ( (TBlue == 0 || TRed ==0) && SBlue != 0 && SRed != 0) {
+            ans += (Math.min(TBlue, TRed) + 1) + " " + (Math.max(SBlue, SRed) + 1);
+        } else if ( (SBlue == 0 || SRed ==0) && TBlue != 0 && TRed != 0) {
+            ans += (Math.max(TBlue, TRed) + 1) + " " + (Math.min(SBlue, SRed) + 1);
+        } else {
+            ans += (Math.max(TBlue, TRed) + 1) + " " + (Math.max(SBlue, SRed) + 1);
+        }
+
+        return ans;
+    }
 }
--- src/test/java/ru/marinin/TestB.java
+++ src/test/java/ru/marinin/TestB.java
@@ -1,4 +1,74 @@
 package ru.marinin;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.marinin.training60_lesson1.B;
+
 public class TestB {
+    @Test
+    @DisplayName("OK")
+    void okTest() {
+        int TBlue = 6;
+        int TRed = 2;
+        int SBlue = 7;
+        int SRed = 3;
+
+        Assertions.assertEquals(B.findMin(TBlue, TRed, SBlue, SRed), "3 4");
+    }
+
+    @Test
+    @DisplayName("one zero elem")
+    void zeroTest() {
+        int TBlue = 6;
+        int TRed = 0;
+        int SBlue = 7;
+        int SRed = 3;
+
+        Assertions.assertEquals(B.findMin(TBlue, TRed, SBlue, SRed), "1 4");
+    }
+
+    @Test
+    @DisplayName("two zero elem")
+    void twoZeroTest() {
+        int TBlue = 6;
+        int TRed = 0;
+        int SBlue = 7;
+        int SRed = 0;
+
+        Assertions.assertEquals(B.findMin(TBlue, TRed, SBlue, SRed), "1 1");
+    }
+
+    @Test
+    @DisplayName("dif max elem")
+    void difMaxTest() {
+        int TBlue = 6;
+        int TRed = 1;
+        int SBlue = 1;
+        int SRed = 6;
+
+        Assertions.assertEquals(B.findMin(TBlue, TRed, SBlue, SRed), "2 2");
+    }
+
+    @Test
+    @DisplayName("one zero dif max elem")
+    void zeroDifMaxTest() {
+        int TBlue = 6;
+        int TRed = 0;
+        int SBlue = 3;
+        int SRed = 7;
+
+        Assertions.assertEquals(B.findMin(TBlue, TRed, SBlue, SRed), "1 8");
+    }
+
+    @Test
+    @DisplayName("one zero dif max 2 elem")
+    void zeroDifMax2Test() {
+        int TBlue = 3;
+        int TRed = 5;
+        int SBlue = 6;
+        int SRed = 0;
+
+        Assertions.assertEquals(B.findMin(TBlue, TRed, SBlue, SRed), "6 1");
+    }
 }
