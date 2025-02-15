fc69552e07e335b7cba2e98fa3c69af8a207c7e7
Oleg <oleg.marinin.49@yandex.ru>
2025-02-15 18:20:16 +0300

    OK without tests

--- src/main/java/ru/marinin/training60_lesson1/B.java
+++ src/main/java/ru/marinin/training60_lesson1/B.java
@@ -1,5 +1,7 @@
 package ru.marinin.training60_lesson1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class B {
@@ -12,23 +14,45 @@ public class B {
         int SBlue = scanner.nextInt();
         int SRed = scanner.nextInt();
 
-        System.out.println(findMin(TBlue, TRed, SBlue, SRed));
-    }
-
-    public static String findMin(int TBlue, int TRed, int SBlue, int SRed) {
-        String ans = "";
+        List<Integer> list = new ArrayList<>();
 
 
-        if ((TBlue > TRed && SBlue > SRed) || (TBlue < TRed && SBlue < SRed)) {
-            ans += (Math.min(TBlue, TRed) + 1) + " " + (Math.min(SBlue, SRed) + 1);
-        } else if ((TBlue == 0 && SBlue >= SRed) || (TRed == 0 && SBlue <= SRed)) {
-            ans += (Math.min(TBlue, TRed) + 1) + " " + (Math.max(SBlue, SRed) + 1);
-        } else if ((SBlue == 0 && TBlue >= TRed) || (SRed == 0 && TBlue <= TRed)) {
-            ans += (Math.max(TBlue, TRed) + 1) + " " + (Math.min(SBlue, SRed) + 1);
-        } else {
-            ans += (Math.min(TBlue, TRed)) + " " + (Math.max(SBlue, SRed) + 1);
+        if (TBlue > 0 && TBlue > 0) {
+            list.add(TRed + 1);
+            list.add(SRed + 1);
         }
 
-        return ans;
+        if (TRed > 0 && TRed > 0) {
+            list.add(TBlue + 1);
+            list.add(SBlue + 1);
+        }
+
+        if (TBlue > 0 && TRed > 0) {
+            list.add(Math.max(TBlue, TRed) + 1);
+            list.add(1);
+        }
+
+        if (SBlue > 0 && SRed > 0) {
+            list.add(1);
+            list.add(Math.max(SBlue, SRed) + 1);
+        }
+
+        int imin = 0;
+        int jmin = 1;
+        int min = list.get(0) + list.get(1);
+
+        for (int k = 1; k < list.size()/2 ; k++) {
+            int i = k * 2;
+            int j = i + 1;
+            if (list.get(i) + list.get(j) < min) {
+                imin = i;
+                jmin = j;
+                min = list.get(i) + list.get(j);
+            }
+        }
+
+        System.out.println(list.get(imin) + " " + list.get(jmin));
+
     }
+
 }
