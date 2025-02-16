f494c4283c73d2f0841dfee594a9e3e122d83669
Oleg <oleg.marinin.49@yandex.ru>
2025-02-16 19:11:30 +0300

    task C the other one way

--- src/main/java/ru/marinin/training60_lesson1/C.java
+++ src/main/java/ru/marinin/training60_lesson1/C.java
@@ -10,127 +10,45 @@ public class C {
         int n = scanner.nextInt();
         scanner.nextLine();
 
-        String[][] str = new String[n][n];
+        String[][] strings = new String[n][n];
 
         for (int i = 0; i < n; i++) {
             String[] line = scanner.nextLine().split("");
-            System.arraycopy(line, 0, str[i], 0, n);
+            System.arraycopy(line, 0, strings[i], 0, n);
         }
 
-        System.out.println(getLetter(getPoints(n, str)));
+        getSmallMatrix(n, strings);
 
 
     }
 
-    public static List<Point> getPoints(int n, String[][] str) {
-        List<Point> pointList = new ArrayList<>();
+    public static String[][] getSmallMatrix(int n, String[][] strings) {
+        removeLines(n, strings);
 
-        pointList.add(new Point(0, 0));
-        pointList.add(new Point(n - 1, n - 1));
+        return strings;
+    }
 
-
-        for (int i = n - 1; i >= 0; i--) {
+    public static String[][] removeLines(int n, String[][] strings) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String line = "";
             for (int j = 0; j < n; j++) {
-                if (str[i][j].equals("#")) {
-                    int x = j;
-                    int y = n - i - 1;
-                    pointList.add(new Point(x, y));
-                    while (j < n - 1 && str[i][j + 1].equals("#")) j++;
-                    while (i > 0 && str[i - 1][j].equals("#")) i--;
-                    if (x != j || y != (n - i - 1)) {
-                        x = j;
-                        y = n - i - 1;
-                        pointList.add(new Point(x, y));
-                    }
-
-                }
+                line += strings[i][j];
+            }
+            if (line.indexOf("#") > 0 && (list.isEmpty() || !list.getLast().equals(line))) {
+                list.add(line);
             }
         }
-
-        return pointList;
-    }
-
-    public static String getLetter(List<Point> pointList) {
-        if (pointList.size() > 6) return "X";
-        if (pointList.size() == 6) {
-            if (    (pointList.get(0).x < pointList.get(2).x) &&
-                    (pointList.get(2).x == pointList.get(4).x) &&
-                    (pointList.get(4).x < pointList.get(5).x) &&
-                    (pointList.get(5).x < pointList.get(3).x) &&
-                    (pointList.get(3).x == pointList.get(1).x) &&
-                    (pointList.get(0).y == pointList.get(2).y) &&
-                    (pointList.get(2).y < pointList.get(3).y) &&
-                    (pointList.get(3).y < pointList.get(4).y) &&
-                    (pointList.get(4).y < pointList.get(5).y) &&
-                    (pointList.get(5).y < pointList.get(1).y)
-            ) return "P";
-             else if (  (pointList.get(0).x < pointList.get(2).x) &&
-                        (pointList.get(2).x == pointList.get(4).x) &&
-                        (pointList.get(4).x < pointList.get(3).x) &&
-                        (pointList.get(3).x == pointList.get(5).x) &&
-                        (pointList.get(5).x < pointList.get(1).x) &&
-                        (pointList.get(0).y == pointList.get(2).y) &&
-                        (pointList.get(2).y < pointList.get(3).y) &&
-                        (pointList.get(3).y < pointList.get(4).y) &&
-                        (pointList.get(4).y < pointList.get(5).y) &&
-                        (pointList.get(5).y == pointList.get(1).y)
-            ) return "H";
-             else return "X";
+        if (list.isEmpty()) {
+            return new String[0][0];
+        } else {
+            return new String[list.size()][list.getFirst().length()];
         }
-        if (pointList.size() == 4) {
-            if (    (pointList.get(0).x < pointList.get(2).x) &&
-                    (pointList.get(2).x < pointList.get(3).x) &&
-                    (pointList.get(3).x == pointList.get(1).x) &&
-                    (pointList.get(0).y < pointList.get(2).y) &&
-                    (pointList.get(2).y < pointList.get(3).y) &&
-                    (pointList.get(3).y == pointList.get(1).y)
-            ) return "L";
-            else if (   (pointList.get(0).x < pointList.get(2).x) &&
-                        (pointList.get(2).x < pointList.get(3).x) &&
-                        (pointList.get(3).x == pointList.get(1).x) &&
-                        (pointList.get(0).y < pointList.get(2).y) &&
-                        (pointList.get(2).y < pointList.get(3).y) &&
-                        (pointList.get(3).y < pointList.get(1).y)
-            ) return  "C";
-            else if (   (pointList.get(0).x < pointList.get(2).x) &&
-                        (pointList.get(2).x < pointList.get(3).x) &&
-                        (pointList.get(3).x < pointList.get(1).x) &&
-                        (pointList.get(0).y < pointList.get(2).y) &&
-                        (pointList.get(2).y < pointList.get(3).y) &&
-                        (pointList.get(3).y < pointList.get(1).y)
-            ) return "O";
-            else if (false)
-                return "I";
-            else
-                return "X";
-        }
-        return " ";
     }
 
-    public static List<Point> getPointList(int[] ints) {
-        List<Point> pointList = new ArrayList<>();
-        for (int i = 0; i < ints.length - 1; i += 2) {
-            pointList.add(new Point(ints[i], ints[i + 1]));
-        }
-        return pointList;
-    }
-}
-
-
-class Point {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public static String[][] removeColumns(int n, String[][] strings) {
+        return new String[0][0];
     }
 
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
+
 }
