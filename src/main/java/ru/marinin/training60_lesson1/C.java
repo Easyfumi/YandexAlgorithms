03e22cc2e12ce73f977eced254827edc5d19caa8
Oleg <oleg.marinin.49@yandex.ru>
2025-02-16 14:11:17 +0300

    task C getPoints method

---
+++ src/main/java/ru/marinin/training60_lesson1/C.java
@@ -0,0 +1,74 @@
+package ru.marinin.training60_lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        String[][] str = new String[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] line = scanner.nextLine().split("");
+            System.arraycopy(line, 0, str[i], 0, n);
+        }
+
+        getPoints(n, str);
+
+
+    }
+
+    public static List<Point> getPoints(int n, String[][] str) {
+        List<Point> pointList = new ArrayList<>();
+
+        pointList.add(new Point(0, 0));
+        pointList.add(new Point(n-1, n-1));
+
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (str[i][j].equals("#")) {
+                    int x = j;
+                    int y = n - i - 1;
+                    pointList.add(new Point(x,y));
+                    while (j<n && str[i][j+1].equals("#")) j++;
+                    while (i>0 && str[i-1][j].equals("#")) i--;
+                    x = j;
+                    y = n - i - 1;
+                    pointList.add(new Point(x,y));
+                }
+            }
+        }
+
+        return pointList;
+    }
+}
+
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
---
+++ src/test/java/ru/marinin/TestC.java
@@ -0,0 +1,5 @@
+package ru.marinin;
+
+public class TestC {
+    void
+}
